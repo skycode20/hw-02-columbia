3b3606e59bec87408820be7b6034d5a8da8bd7f0
Skyler R <skyler.rencher@gmail.com>
2020-12-07 01:28:33 -0500

    added the isValid function to check if the fields meet the conditions defined in the functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
 const isNotEmpty = (value) => {
     if (value == null || typeof value == 'undefined') return false;
     return (value.length > 0);
-}
+};
 
 const isEmail = (email) => {
     let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9]{0,61}[a-zA-Z0-9])?)*$/;
     return regex.test(String(email).toLowerCase());
+};
+
+const fieldValidation = (field, validationFunction) => {
+    if (field == null) return false;
+
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+        field.className = 'placeholderRed';
+    } else {
+        field.className = '';
+    }
+
+    return isFieldValid;
+};
+
+const isValid = () => {
+    var valid = true;
+
+    valid &= fieldValidation(fields.name, isNotEmpty);
+    valid &= fieldValidation(fields.email, isEmail);
+    valid &= fieldValidation(fields.message, isNotEmpty);
+
+    return valid;
 }
