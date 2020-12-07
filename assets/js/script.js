f6076dca95a035a1dcfef2f0c74cac2265394d15
Skyler R <skyler.rencher@gmail.com>
2020-12-07 01:18:49 -0500

    added isEmail func that checks if the email address format is valid.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,8 @@ const isNotEmpty = (value) => {
     if (value == null || typeof value == 'undefined') return false;
     return (value.length > 0);
 }
+
+const isEmail = (email) => {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+}
